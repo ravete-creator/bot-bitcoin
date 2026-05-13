d1cf5b093acb2289ce2808dca02c8dcbb68833eb
ravete-creator <wesley0297@gmail.com>
2026-05-13 13:02:34 -0300

    primeiro commit

---
+++ bot.py
@@ -0,0 +1,36 @@
+import requests
+import time
+
+TOKEN = "8970298887:AAG6tLyBCnTNMLJkqSZIbd45NeLqJUDJk8I"
+CHAT_ID = "929309450"
+
+def pegar_preco_bitcoin():
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+
+    resposta = requests.get(url)
+    dados = resposta.json()
+
+    preco = dados["price"]
+
+    return preco
+
+def enviar_mensagem(mensagem):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    dados = {
+        "chat_id": CHAT_ID,
+        "text": mensagem
+    }
+
+    requests.post(url, data=dados)
+
+while True:
+    preco = pegar_preco_bitcoin()
+
+    mensagem = f"Bitcoin agora: ${preco}"
+
+    enviar_mensagem(mensagem)
+
+    print(mensagem)
+
+    time.sleep(60)
